5ee70726aa8a49c20ca8ce3acdf5627338112fbc
Yui <107255849+TendoYuki@users.noreply.github.com>
2022-06-16 20:09:26 +0200

    Add files via upload

--- onepiece.js
+++ onepiece.js
@@ -44,3 +44,27 @@ function calculer(){
 	reste.textContent = " N'hesitez pas à aller essayer notre qcm sur Tokyo Ghoul juste en dessous 👇 ";
 	
 }
+audioPlayer();
+function audioPlayer(){
+	var currentSong = 0;
+	$("#audioPlayer")[0].src = $("#playlist li a") [0];
+	$("#audioPlayer")[0].play();
+	$("#playlist li a").click(function(e){
+		e.preventDefault();
+		$("#audioPlayer")[0].src = this;
+		$("#audioPlayer")[0].play();
+		$("#playlist li").removeClass("current-song");
+		currentSong = $(this).parent().index();
+		$(this).parent().addClass("current-song");
+		});
+				
+	$("#audioPlayer")[0].addEventListener("ended",function(){
+	currentSong++;
+	if(currentSong == $("#playlist li a").length)
+						currentSong = 0;
+	$("#playlist li").removeClass("current-song");
+	$("#playlist li:eq("+currentSong+")").addClass("current-song");
+	$("#audioPlayer")[0].src = $("#playlist li a")[currentSong].href;
+	$("#audioPlayer")[0].play();
+	});
+}
